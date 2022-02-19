9e73fdac31f0dd08495607d9a5e482f79b67f331
Osawa.ryu <60128482+K656l@users.noreply.github.com>
2022-02-20 02:09:16 +0700

    Create page
    portfolio & dashboard page

--- 6210743115/blog/routes/web.php
+++ 6210743115/blog/routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('StoreInd.index');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
