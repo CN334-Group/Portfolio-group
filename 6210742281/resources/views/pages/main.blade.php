a687c16d3d9af459768fe25f73bf45598e8590dc
monosik <tot74800@gmail.com>
2022-02-20 14:38:44 +0700

    Create Model and update front-Ent Portfolio Web
    Create Model to show data from Database to Dashboard and update Data front-Ent Database

--- 6210742281/resources/views/pages/main.blade.php
+++ 6210742281/resources/views/pages/main.blade.php
@@ -7,7 +7,33 @@
                 <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
                 <li class="breadcrumb-item active">Main</li>
             </ol>
-            <h2>hello</h2>
+            {{-- Form for Update Main --}}
+            <div class="row">
+                {{-- Update Background Image --}}
+                <div class="from-group col-md3 mt-3">
+                    <h3>Background Image</h3>
+                    <img style="height: 30vh" src="{{url($main->bc_img)}}" class="img-thumbnail">
+                    <input class="mt-3" type="file" id="bc_img" name="bc_img">
+                </div>
+
+                {{-- Update Title and Sub Title --}}
+                <div class="form-group col-md4 mt-3">
+                    <div class="mb-3">
+                        <label for="title"><h4>Title</h4></label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{$main->title}}">
+                    </div>
+                    <div class="mb-5"> 
+                        <label for="title"><h4>Sub Title</h4></label>
+                        <input type="text" class="form-control" id="sub_title" name="sub_title"
+                        value="{{$main->sub_title}}">
+                    </div>
+
+                    <div>
+                        <h4>Upload Resume</h4>
+                        <input class="mt-2" type="file" id="resume" name="resume"> 
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 @endsection
--- 6210742281/routes/web.php
+++ 6210742281/routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@index')->name('home');
 Route::get('/admin/dashboard', 'PagesController@dashboard')->name('admin.dashboard');
-Route::get('/admin/main', 'PagesController@main')->name('admin.main');
+Route::get('/admin/main', 'MainPagesController@index')->name('admin.main');
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
