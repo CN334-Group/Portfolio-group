bdbc2965799fbd0e6df97a0d1f01eca4db5b6ef4
FarLaLook <fluke44412@gmail.com>
2022-02-21 13:49:35 +0700

    Add Service System

--- 6210742224/cat/routes/web.php
+++ 6210742224/cat/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', 'PagesController@index')->name('home');
 Route::get('/admin/dashboard', 'PagesController@dashboard')->name('admin.dashboard');
 Route::get('/admin/main', 'MainPagesController@index')->name('admin.main');
 Route::put('/admin/main', 'MainPagesController@update')->name('admin.main.update');
+Route::get('/admin/services/create', 'ServicePagesController@create')->name('admin.services.create');
+Route::post('/admin/services/create', 'ServicePagesController@store')->name('admin.services.store');
 
 Auth::routes();
 
