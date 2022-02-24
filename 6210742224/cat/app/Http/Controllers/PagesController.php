3f4ad145898f4a9bbf279a04567ab00a06f1640b
FarLaLook <fluke44412@gmail.com>
2022-02-24 21:27:23 +0700

    Create Port System

--- 6210742224/cat/app/Http/Controllers/PagesController.php
+++ 6210742224/cat/app/Http/Controllers/PagesController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Main;
 use App\Models\Service;
+use App\Models\Portfolio;
 
 class PagesController extends Controller
 {
     public function index(){
         $main = Main::first();
         $services = Service::all();
-        return view('Test.index', compact('main', 'services'));
+        $portfolios = Portfolio::all();
+        return view('Test.index', compact('main','services','portfolios'));
     }
+
+
 
     public function dashboard(){
         return view('Test.dashboard');
